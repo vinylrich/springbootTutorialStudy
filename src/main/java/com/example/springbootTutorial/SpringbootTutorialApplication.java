0a51e5a36a759cb0ea4ef07aa69c78e99daf97dd
vinylrich <whktjd0109@gmail.com>
2024-02-08 20:29:39 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootTutorial/SpringbootTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTutorialApplication.class, args);
+	}
+
+}
+
